fa6f9c51c3d8ef91ecd0f463f7c9dfc6af8bf710
Priyangshu Das <priyangshud924@gmail.com>
2026-07-14 16:07:50 +0530

    feat(script): add variable declarations and object logging examples

--- JavaScript-Learning/Chapter-01-Variables-and-DataTypes/script.js
+++ JavaScript-Learning/Chapter-01-Variables-and-DataTypes/script.js
@@ -1,2 +1,24 @@
 console.log("hello world")
 console.log("hello bachoo")
+var a = 100
+let b = 'Priyang'
+const author = "PRIYANG"
+console.log(a, b, author)
+//Primitive Data Types
+let c = null;
+let d = 786;
+let e = "STRING"
+let f = BigInt("420")
+let g = Symbol("THE SYMBOL")
+let h = true;
+let i
+console.log(c, d, e, f, g, h, i)
+console.log(typeof f)
+
+// Objects in JS 
+const item = {
+    "Harry": true,
+    "Priyang": false,
+    "Mana": 157
+}
+console.log(item["Mana"])
