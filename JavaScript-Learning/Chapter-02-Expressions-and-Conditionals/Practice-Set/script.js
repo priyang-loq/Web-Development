6fccac7b0d55cadc465809b5136e49f99d54f9c4
Priyangshu Das <priyangshud924@gmail.com>
2026-07-15 01:11:10 +0530

    feat: add index.html and script.js for Chapter 2 practice set

---
+++ JavaScript-Learning/Chapter-02-Expressions-and-Conditionals/Practice-Set/script.js
@@ -0,0 +1,17 @@
+// Q1
+// let a = prompt("Enter your Age")
+// a = Number.parseInt(a)
+// if( a > 10 && a < 20)
+// {
+//     alert("Your age is between 10 and 20")
+// }
+// else alert("you are not lie on the age boundary")
+// Q3 
+// Q4
+let a = prompt("Enter the number to check it is divisible by 2 or 3")
+a = Number.parseInt(a)
+if( a%2 == 0 || a%3 ==0)
+{
+    alert("The number is divisible by 2 or 3")
+}
+else alert("The number is not divisible by 2 or 3")
