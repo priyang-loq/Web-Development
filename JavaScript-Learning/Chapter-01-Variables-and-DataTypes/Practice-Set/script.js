af8cb1bb174d7c3abd9331485cdf3bcb3179f64f
Priyangshu Das <priyangshud924@gmail.com>
2026-07-14 16:44:48 +0530

    feat(script): solved Q1,Q2,Q3,Q4

--- JavaScript-Learning/Chapter-01-Variables-and-DataTypes/Practice-Set/script.js
+++ JavaScript-Learning/Chapter-01-Variables-and-DataTypes/Practice-Set/script.js
@@ -1,6 +1,14 @@
 // Q1
 let a = "rohon"
-let b =123
+let b = 123
 console.log(a + b)
 // Q2
-console.log(typeof a)
+console.log(typeof a + b)
+// Q3
+const c = {
+    nmae: "Priyang",
+    section: 1
+}
+// Q4
+c['freind'] = "mosom"
+console.log(c)
