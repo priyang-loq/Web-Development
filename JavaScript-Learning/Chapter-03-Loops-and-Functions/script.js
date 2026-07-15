03d71210a49887ada5961f8845eddef547cc9c5b
Priyangshu Das <priyangshud924@gmail.com>
2026-07-15 10:57:49 +0530

    feat: add index.html and script.js for Chapter 3 on loops and functions

---
+++ JavaScript-Learning/Chapter-03-Loops-and-Functions/script.js
@@ -0,0 +1,10 @@
+// For-in Loops
+let obj = {
+    rohon : 45,
+    rahul : 46,
+    pratiksha : 50,
+    priyang : -99
+}
+for (let a in obj){
+    console.log("Marks of "+ a + " are " + obj[a])
+}
