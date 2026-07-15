37a6f2f7e730f06398511fca0a016715bc6d5f55
Priyangshu Das <priyangshud924@gmail.com>
2026-07-15 12:05:57 +0530

    feat(script): refactor object iteration examples for clarity and consistency

--- JavaScript-Learning/Chapter-03-Loops-and-Functions/Practice-Set/script.js
+++ JavaScript-Learning/Chapter-03-Loops-and-Functions/Practice-Set/script.js
@@ -1,9 +1,13 @@
 // Q1
 let obj = {
-    harsh : 97,
-    hari : 98,
-    hore : 99
+    harsh: 97,
+    hari: 98,
+    hore: 99
 }
-for ( let a in obj){
-    console.log("marks of "+a+" is "+obj[a])
+for (let i = 0; i < Object.keys(obj).length; i++) {
+    console.log("the marks of " + Object.keys(obj)[i] + " are " + obj[Object.keys(obj)[i]])
+}
+// Q2
+for (let a in obj) {
+    console.log("marks of " + a + " is " + obj[a])
 }
