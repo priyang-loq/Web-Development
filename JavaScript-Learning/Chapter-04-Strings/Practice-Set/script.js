1ddfe35485da7c52de03165dc3ea4ba8262b7f30
Priyangshu Das <priyangshud924@gmail.com>
2026-07-15 13:40:21 +0530

    feat: add index.html and script.js for initial setup of Practice Set

---
+++ JavaScript-Learning/Chapter-04-Strings/Practice-Set/script.js
@@ -0,0 +1,11 @@
+// Q2
+let str = "Code With Harry";
+
+console.log(str.includes("With"));     // true
+console.log(str.includes("Java"));     // false
+
+console.log(str.startsWith("Code"));   // true
+console.log(str.startsWith("With"));   // false
+
+console.log(str.endsWith("Harry"));    // true
+console.log(str.endsWith("Code"));     // false
