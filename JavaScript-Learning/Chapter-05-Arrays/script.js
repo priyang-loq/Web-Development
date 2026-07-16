5c3f1c632c79326d5b5ed0ba38bfdb715853a0d9
Priyangshu Das <priyangshud924@gmail.com>
2026-07-16 15:53:04 +0530

    feat(script): add array toString method example in script.js

--- JavaScript-Learning/Chapter-05-Arrays/script.js
+++ JavaScript-Learning/Chapter-05-Arrays/script.js
@@ -5,5 +5,17 @@ console.log(arr[1])
 console.log(arr[2])
 console.log(arr[3])
 console.log(arr[4])
+// Array Methods
+// let r = arr.toString()
 
+// let r = arr.join("and")
 
+// let r = arr.pop()
+
+// let r = arr.push(6)
+
+// let r = arr.shift()
+
+// let r = arr.unshift(7)
+
+console.log(r)
