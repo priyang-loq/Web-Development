9b721e26ded861cf13e365b7aa497c40cbdfc180
Priyangshu Das <priyangshud924@gmail.com>
2026-07-17 21:07:58 +0530

    feat(script): add initial array exercises and examples in script.js

---
+++ JavaScript-Learning/Chapter-05-Arrays/Practice-Set/script.js
@@ -0,0 +1,44 @@
+// Q1
+// let arr = [];
+
+// for (let i = 0; i < 4; i++) {
+//     arr.push(Number(prompt("Enter the numbers: ")));
+// }
+
+// console.log(arr);
+
+// Q2
+// let arr = [];
+
+// while (true) {
+//     const num = Number(prompt("Enter the numbers (0 to stop): "));
+
+//     if (num === 0) break;
+
+//     arr.push(num);
+// }
+
+// Q3
+// const arr = [1, 2, 10, 20, 40, 100, 60];
+
+// const divisibleBy10 = (num) => {
+//     return num % 10 === 0;
+// };
+
+// let result = arr.filter(divisibleBy10);
+
+// console.log(result);
+
+// Q4
+// const arr = [1,2,3,4,5];
+// let sqrarr = arr.map((element) =>{
+//     return element*element;
+// });
+// console.log(sqrarr);
+
+// Q5
+// const n = [1,2,3,4,5,6];
+// let factorial = n.reduce((num1,num2) =>{
+//     return num1*num2;
+// });
+// console.log(factorial);
