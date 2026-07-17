14aee98ec267801010cbfa8639e71023e7ac2d9d
Priyangshu Das <priyangshud924@gmail.com>
2026-07-18 00:39:16 +0530

    feat(script): update script.js to include user interaction prompts

--- JavaScript-Learning/Chapter-06-JavaScript-in-The-Browser/script.js
+++ JavaScript-Learning/Chapter-06-JavaScript-in-The-Browser/script.js
@@ -1,8 +1,20 @@
-console.log(console);
-console.clear();
-console.assert|(5>56);
-obj = {a:1,b:3,c:5};
-console.table(obj);
-console.warn("warning");
-console.error("eroor");
-console.info("info0");
+// console.log(console);
+// console.clear();
+// console.assert|(5>56);
+// obj = {a:1,b:3,c:5};
+// console.table(obj);
+// console.warn("warning");
+// console.error("eroor");
+// console.info("info0");
+
+alert("Welcome!");
+
+let name = prompt("Enter your name:");
+
+let permission = confirm("Do you want to continue?");
+
+if (permission) {
+    alert("Hello " + name + "! Let's start.");
+} else {
+    alert("Goodbye " + name + "!");
+}
