6ed511de11c3ca6f9a6b653b0b32cd9beb9e8181
Priyangshu Das <priyangshud924@gmail.com>
2026-07-18 23:16:16 +0530

    Solve JavaScript Chapter 6 practice set

---
+++ JavaScript-Learning/Chapter-06-JavaScript-in-The-Browser/Practice-Set/script.js
@@ -0,0 +1,65 @@
+// Q1
+// let age = Number.parseInt(prompt("Enter your age:"));
+
+// if (age >= 18) {
+//     alert("You can Drive.");
+// } else {
+//     alert("You can't Drive!");
+// }
+
+// Q2
+// let want;
+
+// do {
+//     let age = Number.parseInt(prompt("Enter your age:"));
+
+//     if (age >= 18) {
+//         alert("You can Drive.");
+//     } else {
+//         alert("You can't Drive!");
+//     }
+
+//     want = confirm("Do you want to see the prompt again?");
+// } while (want);
+
+// Q3
+// let want;
+
+// do {
+//     let age = Number.parseInt(prompt("Enter your age:"));
+//     if (age < 0){
+//         console.error("Age enterd is negative");
+//         break;
+//     }
+//     if (age >= 18) {
+//         alert("You can Drive.");
+//     } else {
+//         alert("You can't Drive!");
+//     }
+
+//     want = confirm("Do you want to see the prompt again?");
+// } while (want);
+
+// Q4
+// let want;
+
+// do {
+//     let age = Number.parseInt(prompt("Enter your age:"));
+//     if (age < 0){
+//         console.error("Age enterd is negative");
+//         break;
+//     }
+//     if(age > 4){
+//         location.href = "https://www.google.com/";
+//     }
+//     if (age >= 18) {
+//         alert("You can Drive.");
+//     } else {
+//         alert("You can't Drive!");
+//     }
+
+//     want = confirm("Do you want to see the prompt again?");
+// } while (want);
+
+// Q5
+// document.body.style.background = "blue";
