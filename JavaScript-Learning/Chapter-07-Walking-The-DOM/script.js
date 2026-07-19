dda97941db9964540df0dac0a4fedb8fff1be094
Priyangshu Das <priyangshud924@gmail.com>
2026-07-19 13:00:31 +0530

    feat: add table structure and JavaScript functionality for Chapter 07 - Walking The DOM

---
+++ JavaScript-Learning/Chapter-07-Walking-The-DOM/script.js
@@ -0,0 +1,8 @@
+console.log(document.body);
+console.log(document.body.firstElementChild);
+let arr = Array.from(document.body.childNodes);
+console.log(arr);
+console.log(document.head.nextElementSibling);
+let t = document.body.firstElementChild;
+console.log(t);
+console.log(t.rows);
