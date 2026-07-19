1c03940d769e0a25e472681a822391c4ca846bd7
Priyangshu Das <priyangshud924@gmail.com>
2026-07-19 15:23:19 +0530

    feat: add JavaScript functionality to manipulate navbar element color

---
+++ JavaScript-Learning/Chapter-07-Walking-The-DOM/Practice-Set/script.js
@@ -0,0 +1,4 @@
+// Q1 
+let firstElementofNavbar = document.body.firstElementChild.firstElementChild;
+console.log(firstElementofNavbar);
+firstElementofNavbar.querySelector("a").style.color = "green";
